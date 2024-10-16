1a352e3114f33067a1a26d91f58bfd6b72124169
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-10-15 23:05:06 -0400

    need to change the3 fruit

--- app/components/VideoList.js
+++ app/components/VideoList.js
@@ -2,3 +2,6 @@
 
 //Role: The VideoList component will render a list of videos provided by the Page component.
 //Stateless: This component will not maintain its own state; it will simply render the list of videos passed down from the Page component
+
+
+
--- app/page.js
+++ app/page.js
@@ -1,24 +1,21 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import { SearchBar } from "./components/searchBar";
+import Apple from "./components/random";
+import { RealFruit, FruitStand } from "./components/random";
 //Role: The Page component will be the topmost (parent) component that renders all other components.
 //State: It will contain the main state object, including a list of videos and the selectedVideo (the currently displayed main video).
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-       
-       
-
-        <div className={styles.ctas}>
-        </div>
-
-      </main>
-
-
-      <footer className={styles.footer}>
-      </footer>
+    <div>
+      <h1>Default Function</h1>
+      <Apple />
+      <br />
+      <br />
+      <h1>Named Functions</h1>
+      <RealFruit />
+      <FruitStand />
     </div>
   );
 }
