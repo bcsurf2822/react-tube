29aab47918675c22e3d672f8e4fd9857fc580cbc
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-10-17 05:24:20 -0400

    at api key

--- app/components/searchBar.js
+++ app/components/searchBar.js
@@ -4,20 +4,21 @@
 //Role: The search bar component will be responsible for capturing user input for video searches.
 //State: It will maintain a single piece of state: term, which represents the search query.
 //Interaction: This term will be passed back to the Page component, which will handle making requests to the server based on the search term.
-
-
 'use client';
 import { useState } from 'react';
 
-//  a named export is different then a default export
-export const SearchBar = () => {
+export const SearchBar = ({ onSearchTermChange }) => {
 	const [term, setTerm] = useState('');
-
-	console.log(term); // For debugging, this will log the current search term to the console
 
 	return (
 		<div className='search-bar'>
-			<input onChange={(e) => setTerm(e.target.value)} value={term} />
+			<input
+				value={term}
+				onChange={(event) => {
+					setTerm(event.target.value);
+					onSearchTermChange(event.target.value);
+				}}
+			/>
 		</div>
 	);
 };
--- app/page.js
+++ app/page.js
@@ -1,21 +1,19 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { SearchBar } from "./components/searchBar";
-import Apple from "./components/random";
-import { RealFruit, FruitStand } from "./components/random";
-//Role: The Page component will be the topmost (parent) component that renders all other components.
-//State: It will contain the main state object, including a list of videos and the selectedVideo (the currently displayed main video).
+'use client';
+import { SearchBar } from './components/searchBar';
+const API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
+
+//.env info Most of the time we have 2 sets of keys - one set for each environment. So one set for "development" (to use to test your app while you're building it) and another for "production" (for you app to use once it's deployed for the world to see).
+
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Default Function</h1>
-      <Apple />
-      <br />
-      <br />
-      <h1>Named Functions</h1>
-      <RealFruit />
-      <FruitStand />
-    </div>
-  );
+	const videoSearch = (term) => {
+
+		console.log(term);
+	};
+
+	return (
+		<div className='row justify-content-center'>
+			<SearchBar onSearchTermChange={videoSearch} />
+		</div>
+	);
 }
